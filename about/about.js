16cf798489cc3f55807555284a5692e234744301
weakit <vsroshan86@gmail.com>
2018-05-14 19:19:29 +0530

    Initial Commit

---
+++ about/about.js
@@ -0,0 +1,85 @@
+$(document).ready(function () {
+    $('div#overlay').fadeOut();
+});
+
+function _calculateAge(birthday) {
+  var ageDifMs = Date.now() - birthday.getTime();
+  var ageDate = new Date(ageDifMs);
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+var d = new Date('09-09-2003');
+age = _calculateAge(d).toString();
+
+var full = "";
+
+if (age < 17) {
+  full = "only " + age;
+}
+else{
+  full = age;
+}
+
+console.log(full);
+
+$("#age").text(full);
+
+var dark = false;
+
+function darkness(){
+  $('link[href="alight.css"]').attr('href','adark.css');
+}
+
+function light(){
+  $('link[href="adark.css"]').attr('href','alight.css');
+}
+
+$('div#click').click(function (){
+  if (!dark){
+    darkness()
+    dark = true;
+  }
+  else {
+    light()
+    dark = false
+  }
+});
+
+function awkward(){
+  $('body').addClass('awk1');
+  $('html').addClass('awk1');
+  $("#rosh").addClass('awk2');
+  $("#rosh").text("if it's awkward stuff, please try to be as not awkward as possible, otherwise it'll get really awkward and then i not feel like talking to you, and… and, crap this is really awkward. ");
+}
+
+function un_awkward() {
+  $('#rosh').removeClass('awk2');
+  $("#rosh").text("Rosh's stuff");
+  $('body').removeClass('awk1');
+  $('html').removeClass('awk1');
+}
+
+$('.awkward').mousedown(function() {
+  awkward();
+});
+
+
+$('.awkward').mouseup(function() {
+  un_awkward();
+});
+
+$('.awkward').mouseout(function() {
+  un_awkward();
+});
+
+var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+function hasTouch() {
+    return 'ontouchstart' in document.documentElement
+           || navigator.maxTouchPoints > 0
+           || navigator.msMaxTouchPoints > 0;
+}
+
+if (hasTouch()){
+  $("h1.stuff").removeClass('hover');
+}
---
+++ youtube.js
@@ -0,0 +1,68 @@
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '100%',
+    width: '100%',
+    videoId: 'dQw4w9WgXcQ',
+    playerVars: {
+      'autoplay': 1,
+      'controls': 0,
+      'rel': 0,
+      'fs': 0,
+      'showinfo': 0,
+      'loop': 1,
+      'disablekb': 1,
+      'playsinline': 1,
+    },
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+function onPlayerReady(event) {
+  event.target.playVideo();
+  setTimeout(function(){
+    try {$("p#hack").fadeTo(400, 100)}
+    catch(err) {console.log(err);}
+    if (player.getPlayerState() == -1) {
+      player.mute()
+      player.playVideo()
+    }
+    setTimeout(function(){
+      if (player.getPlayerState() == -1) {
+        $("p#click").fadeTo(300, 100)
+      }
+    })
+  }, 2000)
+}
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING) {
+    if(player.isMuted()){
+      $("p#click").fadeTo(300, 100)
+    }
+  }
+  if (event.data == YT.PlayerState.PAUSED) {
+    player.unMute()
+    player.playVideo()
+    $("p#click").fadeTo(200, 0)
+    $("p#click").text('enjoy')
+    $("p#click").fadeTo(200, 1000)
+    setTimeout(function() {$("p#click").fadeTo(800, 0)}, 1500)
+  }
+}
+
+$(document).on('click', '#content', function(){
+  player.unMute()
+  player.playVideo()
+  $("p#click").fadeTo(200, 0)
+  $("p#click").text('enjoy')
+  $("p#click").fadeTo(200, 1000)
+  setTimeout(function() {$("p#click").fadeTo(800, 0)}, 1500)
+})
