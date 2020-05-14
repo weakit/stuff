d5b70e597a078da85fd20abe4cbf1a49ea380fd0
weakit <vsroshan86@gmail.com>
2020-05-14 16:25:52 +0530

    all new

---
+++ js.js
@@ -0,0 +1,47 @@
+var time;
+var json;
+
+function getTimeStr() {
+    var t = new Date();
+    return t.toUTCString().slice(17);
+}
+
+function setTimeStr() {
+    $("#time").text(getTimeStr());
+}
+
+function setTitle() {
+    $("#title").text(json["title"]);
+    $("#title").attr("href", json["titleTo"]);
+    $(document).prop("title", json["pageTitle"]);
+}
+
+function setContent() {
+    $("#content").html(json["content"]);
+}
+
+function setEmail() {
+    $("#email").text(json["email"]);
+    $("#email").attr("href", json["emailTo"]);
+}
+
+function setLink() {
+    $("#link").text(json["link"]);
+    $("#link").attr("href", json["linkTo"]);
+}
+
+function loadPage(page) {
+    jQuery.getJSON(page + ".content", function (data) {
+        json = data;
+    });
+    setTimeStr();
+    time = setInterval(setTimeStr, 500);
+    setTimeout(setTitle, 100);
+    setTimeout(setContent, 150);
+    setTimeout(setEmail, 200);
+    setTimeout(setLink, 250);
+}
+
+$(document).ready(function () {
+    loadPage(window.location.href);
+});
