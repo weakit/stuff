37049a63082f2933ddcf051f04845f0b22d56468
weakit <vsroshan86@gmail.com>
2022-01-22 00:24:14 +0530

    revamp hbr

--- src/pages/higher-brain-functions.js
+++ src/pages/higher-brain-functions.js
@@ -1,29 +1,57 @@
 import * as React from 'react'
 import {Helmet} from 'react-helmet'
-import { centre } from '../styles/hbf.module.css'
+import * as Styles from '../styles/hbf.module.css'
 import '../fonts/fira-code/fira_code_default.css'
+import {useEffect, useState} from "react"
+import Youtube from 'react-youtube'
 
-class Redirecter extends React.Component {
-    render() {
-        return <span></span>
-    }
 
-    componentDidMount() {
-        this.timeout = setTimeout(
-            function() {
-                window.location.href = "https://youtu.be/dQw4w9WgXcQ"
-            }, 2000
-        );
-    }
+const Sneaky = () => {
+    const [showLoading, setLoading] = useState(true)
+    const [showVideo, setVideo] = useState(false)
+    const [target, setTarget] = useState(undefined)
+    const [screen, setScreen] = useState(true)
+    const [pity, setPity] = useState(false)
 
-    componentWillUnmount() {
-        clearTimeout(this.timeout)
-    }
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, Math.random() * 750 + 500)
+    }, [])
+
+    useEffect(() => {
+        if (target !== undefined) {
+            target.playVideo()
+            setScreen(false)
+        }
+    }, [target])
+
+    if (showVideo)
+        return <div className={Styles.scrContainer}>
+            <div className={Styles.scr} style={{opacity: screen ? 1 : 0}} onClick={_ => setPity(true)}/>
+            {pity &&
+                <div className={Styles.pity}>
+                    <div><span>It won't stop playing. Tired?&nbsp;Go&nbsp;back&nbsp;<a href="/">home</a>.</span></div>
+                </div>
+            }
+            <Youtube
+                className={Styles.video}
+                videoId='dQw4w9WgXcQ'
+                opts={{playerVars: {controls: 0, disablekb: 1, fs: 0, loop: 1, modestbranding: 1}}}
+                onReady={event => setTarget(event.target)}
+            />
+        </div>
+    if (showLoading)
+        return <h1>PLEASE WAIT</h1>
+
+    return <button className={Styles.button} onClick={() => {
+        setVideo(true)
+    }}>START</button>
 }
 
 const IndexPage = () => {
     return (
-        <div className={centre}>
+        <div>
             <Helmet>
                 <title>Higher Brain Functions</title>
                 <meta name="theme-color" content="#121212"/>
@@ -32,8 +60,13 @@ const IndexPage = () => {
                 <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
             </Helmet>
-            <h1>PLEASE WAIT</h1><Redirecter></Redirecter>
+            <body style={{maxWidth: 'none', height: '100vh', width: '100vw', left: 0, position: 'fixed'}}>
+            <div className={Styles.centreContainer}>
+                <Sneaky/>
+            </div>
+            </body>
         </div>
+
     )
 }
 
